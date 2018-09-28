b6708a58e2ba9880f5d5919c99ea9a058e9b7614
JossCorrea <joseline.correa@gmail.com>
2018-09-28 11:48:00 -0300

    Correção dos bugs associados ao Drag and Drop e adição de funcionalidade de adicionar com enter

--- script/script.js
+++ script/script.js
@@ -2,10 +2,18 @@ const btnAdd = document.getElementById("btnAdd");
 const novaTarefa = document.getElementById("novaTarefa");
 var idNum = 0
 
-btnAdd.addEventListener("click",function(evt){
+novaTarefa.addEventListener("keypress", function(e){
+    if(e.which == 13){
+       adicionar(event)
+    }
+ })
+
+btnAdd.addEventListener("click",adicionar)
+
+function adicionar(evt){
     evt.preventDefault();
     
-    //Validação da tarefa
+    // Validação da tarefa
     if (novaTarefa.value === "" || novaTarefa.value === " " || 
     novaTarefa.value ===  null || novaTarefa.value === undefined || !novaTarefa.value.trim()){
         novaTarefa.focus();
@@ -13,63 +21,82 @@ btnAdd.addEventListener("click",function(evt){
         return false;
     }
     
-    // Adicionar +1 ao número do id
+    // Incrementar id
     idNum++
 
-    // Adicionar nova tarefa
-    const itemLista = document.createElement("div");
+    // Adicionar nova tarefa como item da lista 
+    const itemLista = document.createElement("li");
+    itemLista.className = "tarefas-lista__itens";
+    itemLista.id = idNum;
+    itemLista.setAttribute("draggable","true");
+    itemLista.setAttribute("ondragstart","drag(event)");
+    itemLista.setAttribute("ondragover","allowDrop(event)");
+    itemLista.setAttribute("ondrop","drop(event)");
+    itemLista.setAttribute("droppable","true");
     itemLista.innerHTML = ` 
-    <div class="tarefas-lista__itens" id="${idNum}" ondrop="drop(event)" ondragover="allowDrop(event)" draggable="true" ondragstart="drag(event)" droppable=true>
-        <p class="tarefa-lista__descricao">${novaTarefa.value}</p>
-        <a href="#" class="btn_editar btn" title="Editar tarefa"onclick = "editar(event)">
-            <img src="img/edit.png" alt="editar" >
+        <p class="tarefa-lista__descricao" draggable = false onclick="checar(event)">${novaTarefa.value}</p>
+        <a href="#" class="btn_editar btn" title="Editar tarefa"onclick = "editar(event)"  draggable = false>
+            <img src="img/edit.png" alt="editar"  draggable = false>
         </a>
-        <a href="#" class="btn_remover btn" title="Excluir tarefa" onclick = "excluir(event)">
-            <img src="img/trash.png" alt="remover">
-        </a>
-    </div>`;
+        <a href="#" class="btn_remover btn" title="Excluir tarefa" onclick = "excluir(event)"  draggable = false>
+            <img src="img/trash.png" alt="remover" draggable = false>
+        </a>`;
 
     //Inserir trarefa 
-    const tarefasLista = document.querySelector(".tarefas-lista")
-    tarefasLista.insertBefore(itemLista,tarefasLista.childNodes[0])
 
+    const tarefasLista = document.querySelector(".tarefas-lista")
+    tarefasLista.appendChild(itemLista)
+    
     // Limpar o campo de input após enviar
     novaTarefa.value = null;
-    
-     //Check na tarefa
-     const descricaoTarefa = document.querySelector(".tarefa-lista__descricao")
-     descricaoTarefa.addEventListener("click", function(evt){
-        evt.preventDefault();
-        if (this.className==="tarefa-lista__descricao"){
-            this.className = "tarefa-lista__descricao_checked"
+}
+
+// Excluir tarefa
+    function excluir(ev){
+        const btnDel = document.getElementById(ev.target.closest("li").id)
+        ev.preventDefault();
+        btnDel.remove()
+    }
+
+// Evento de excluir todos os itens da lista
+    const btnExcluir = document.querySelector(".geral__excluir");
+    btnExcluir.addEventListener("click", function(evento) {
+        evento.preventDefault();
+        const tarefasLista = document.querySelector(".tarefas-lista");
+        tarefasLista.innerHTML = ""
+    })
+
+// Editar tarefa (BONUS)
+    function editar(ev){
+        const btnEdt = document.getElementById(ev.target.closest("li").id)
+        ev.preventDefault();
+        novaTarefa.value = btnEdt.children[0].textContent
+        btnEdt.remove()
+    }
+
+ // Check na tarefa
+    function checar(ev){
+        const idTarefa = document.getElementById(ev.target.closest("li").id)
+        const descricaoTarefa=idTarefa.children[0]
+        if (descricaoTarefa.className==="tarefa-lista__descricao"){
+            descricaoTarefa.className = "tarefa-lista__descricao_checked"
         }else{
-            this.className = "tarefa-lista__descricao"
+            descricaoTarefa.className = "tarefa-lista__descricao"
+        }
+    }
+
+// Checar todos os itens da lista
+    const btnChecar = document.querySelector(".geral__checar");
+    btnChecar.addEventListener("click", function(evento) {
+        evento.preventDefault();
+        const tarefas = document.getElementsByTagName("li");
+        for (let item of tarefas){
+            item.children[0].className = "tarefa-lista__descricao_checked";
         }
     })
-   
 
-})
+// //Drag and Drop
 
-// Cria evento de excluir todos os itens da lista
-const btnExcluir = document.querySelector(".geral__excluir");
-btnExcluir.addEventListener("click", function(evento) {
-    evento.preventDefault();
-    const tarefasLista = document.querySelector(".tarefas-lista");
-    tarefasLista.innerHTML = ""
-})
-
-// Cria evento de checar todos os itens da lista
-const btnChecar = document.querySelector(".geral__checar");
-btnChecar.addEventListener("click", function(evento) {
-        evento.preventDefault();
-        const tarefas = document.getElementsByTagName("p");
-        for (let i = 0; i <= tarefas.length; i++) {
-            tarefas[i].className = "tarefa-lista__descricao_checked";
-        }
-})
-
-
-//Drag and Drop
 function allowDrop(ev) {
     ev.preventDefault();
 }
@@ -81,24 +108,13 @@ function drag(ev) {
 function drop(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
-    console.log("origem", data)
-    // console.log("alvo",ev.target.closest("div").index)
-    const tarefasLista = document.querySelector(".tarefas-lista")
-    console.log(ev.target.parentElement)
-    ev.target.parentNode.insertAdjacentElement('beforebegin',document.getElementById(data));
+    var origem = document.getElementById(data);
+    origem.remove()
+ if (ev.target.tagName === "P" || ev.target.tagName === "A" || ev.target.tagName === "IMG"){
+    ev.target.closest("li").insertAdjacentHTML("afterEnd", origem.outerHTML)
+ } else if (ev.target.id !== null) {
+    document.getElementById(ev.target.id).insertAdjacentHTML("afterEnd", origem.outerHTML)
+ }else{
+     return false
+ }
 }
-
-    //Editar tarefa
-    function editar(ev){
-    const btnEdt = document.getElementById(ev.target.closest("div").id)
-    ev.preventDefault();
-    novaTarefa.value = btnEdt.children[0].textContent
-    btnEdt.remove()
-    }
-
-     //Excluir tarefa
-    function excluir(ev){
-    const btnDel = document.getElementById(ev.target.closest("div").id)
-    ev.preventDefault();
-    btnDel.remove()
-    }
